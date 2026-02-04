5f573c3f670791d67a34ae4c66e117e893490e27
Riquelme <rm7629586@gmail.com>
2026-02-04 17:02:31 -0300

    Término da primeira e segunda fase

---
+++ src/main/java/br/dev/riquelme/OSApiApplication/api/controller/ClienteController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.riquelme.OsApiApplication.api.controller;
+
+import br.dev.riquelme.OsApiApplication.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteController {
+    
+    @PersistenceContext
+    private EntityManager manager;
+    
+    @GetMapping("/clientes")
+    public List<Cliente> listas() {
+        
+        
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
+    }
+    
+}
---
+++ src/test/java/br/dev/riquelme/OSApiApplication/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.riquelme.OSApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
